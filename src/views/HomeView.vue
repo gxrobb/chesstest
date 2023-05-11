9dec7ac3a63b3b3128fd457d8d14108ce6ffca75
robb <gxrobb@gmail.com>
2023-05-11 15:44:16 -0400

    more cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HomeComponent from '@/components/HomeComponent.vue';
+import HomeComponent from '../components/HomeComponent.vue';
 
 export default defineComponent({
   name: 'HomeView',
